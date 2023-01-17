9d4f20c68e4a9533e17068515bfd3300703cad19
zeimedee <42755623+zeimedee@users.noreply.github.com>
2023-01-17 16:30:03 +0000

    synchronized queues functions

--- synchronized_queue/main.go
+++ synchronized_queue/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 const (
 	messagePassStart = iota
 	messageTicketStart
@@ -21,6 +26,40 @@ func (queue *Queue) New() {
 	queue.message = make(chan int)
 	queue.queuePass = make(chan int)
 	queue.queueTicket = make(chan int)
+}
+
+func (Queue *Queue) startTicketIssue() {
+	Queue.message <- messageTicketStart
+	<-Queue.queueTicket
+}
+
+func (Queue *Queue) EndTicketIssue() {
+	Queue.message <- messageTicketEnd
+}
+
+func ticketIssue(queue *Queue) {
+	for {
+		time.Sleep(time.Second * 10)
+		queue.startTicketIssue()
+		fmt.Println("Ticket issue starts")
+		time.Sleep(time.Second * 2)
+		fmt.Println("Ticket issue ends")
+		queue.EndTicketIssue()
+	}
+}
+
+func (Queue *Queue) startPass() {
+	Queue.message <- messagePassStart
+	<-Queue.message
+}
+
+func (Queue *Queue) endPass() {
+	Queue.message <- messagePassEnd
+}
+
+func passenger(Queue *Queue) {
+	for {
+	}
 }
 
 func main() {}
